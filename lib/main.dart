318e693ded2e0eafe58d3e937ab1b4e77150aed9
gustavo <gustavo.maia@b2b.com.pt>
2020-05-28 11:53:28 +0100

    Refatorando o código

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,7 @@ void main() {
             child: Icon(Icons.add),
           ),
 
-          body: Column(
-            children: [
-              Card(
-
-                child: ListTile(
-
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100'),
-                  subtitle: Text('1000'),
-
-                ),
-              ),
-              Card(
-
-                child: ListTile(
-
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('200'),
-                  subtitle: Text('1000'),
-
-                ),
-              ),
-            ],
-          ),
+          body: ListaTransferencia()
         ),
 
 
@@ -45,3 +22,52 @@ void main() {
   );
 }
 
+
+class ListaTransferencia extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+        ItemTransferencia(Transferencia(100, 1000)),
+
+        ItemTransferencia(Transferencia(200, 2000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+
+      child: ListTile(
+
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+
+      ),
+    );
+  }
+  
+}
+
+
+class Transferencia{
+
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+
+}
